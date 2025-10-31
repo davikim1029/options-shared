2338b3ff8700061e5457401bb1d5e6098f9119f4
Davis Kim <davikim@umich.edu>
2025-10-31 08:47:31 -0500

    Added timestamp for option feature snapshot

--- shared_options/models.py
+++ shared_options/models.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime,timezone
 
 class OptionFeature(BaseModel):
     symbol: str
     osiKey: str
+    timestamp: datetime = datetime.now(timezone.utc)  # automatically set to current UTC time
     optionType: int  # 1 = CALL, 0 = PUT
     strikePrice: float
     lastPrice: float
